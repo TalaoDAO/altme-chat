93807aff469381217eab7e74e0ad571abda23517
achille1017 <achillemarseille@gmail.com>
2023-02-14 10:08:06 +0100

    db

--- MatrixClient.py
+++ MatrixClient.py
@@ -20,14 +20,14 @@ async def main() -> None:
     #await client.sync_forever(timeout=30000)  # milliseconds
     await client.sync()
     # "Logged in as @alice:example.org device id: RANDOMDID"
-    room=await client.room_create(alias="did787878778789545455677777",invite=["@altme:altme"])
-    room=str(room)
-    roomID=room[len(room)-27:len(room)-2]
-    print(roomID)
+    #room=await client.room_create(alias="did787878778789545455677777",invite=["@altme:altme"])
+    #room=str(room)
+    #roomID=room[len(room)-27:len(room)-2]
+    #print(roomID)
     # If you made a new room and haven't joined as that user, you can use
     #await client.join("#")
-    client.add_event_callback(message_callback, RoomMessageText)
-    while True:
+    #client.add_event_callback(message_callback, RoomMessageText)
+    """while True:
         chat=input("> ")
         await client.room_send(
             # Watch out! If you join an old room you'll see lots of old messages
@@ -35,8 +35,7 @@ async def main() -> None:
             message_type="m.room.message",
             content={"msgtype": "m.text", "body": chat},
         )
-        await client.sync()
-
+        await client.sync()"""
 
 
 asyncio.get_event_loop().run_until_complete(main())
